fdce661cc9a7e987d2ea054575950e77f77af141
StepanKhachikyan1999 <[stepan-khachikyan@mail.ru]>
2023-01-22 16:27:42 +0400

    first commit

---
+++ app/config/db.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  HOST: "localhost",
+  PORT: 27017,
+  DB: "bezkoder_db",
+  MONGO_URL: 'mongodb+srv://Amas1999:Amas1999@cluster0.cc1rmea.mongodb.net/bezkoder_db?retryWrites=true&w=majority'
+};
+
+//export const MONGO_URL = 'mongodb+srv://Amas1999:<Amas1999>@cluster0.cc1rmea.mongodb.net/bezkoder_db?retryWrites=true&w=majority';
